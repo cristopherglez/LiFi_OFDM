de83336b082e9bd4bc3876fc01bea8620e2f0463
Cristopher Glez <cristopherglez@users.noreply.github.com>
2026-03-19 17:23:06 -0600

    Refactor OFDM receiver and update config

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ def get_config():
     Lfft = 128  # FFT length
     cp_length = 16 # Cyclic prefix length
     oversampling_factor = 10  # Note: parameter name in constructor
-    data_frame_length = 29
+    data_frame_length = 20
     lts_repetitions = 9
     sfo_repetitions = 3
 
